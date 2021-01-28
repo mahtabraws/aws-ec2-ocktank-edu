d3d0c9b4e6a20636d04db97ef78a678995cb2f1f
Mahtab <mahtabr@08f8bc618862.ant.amazon.com>
2021-01-28 12:49:39 -0500

    Video Added

--- src/app/index.php
+++ src/app/index.php
@@ -25,36 +25,12 @@ if (isset($_SESSION['user'])) {
 </head>
 
 <body>
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
-                <div class="card card-signin my-5">
-                    <div class="card-body">
-                        <h5 class="card-title text-center">Sign In<h5>
-                                <form class="form-signin" method="POST" action="login.php">
-                                    <div class="form-label-group">
-                                        <input type="email" id="inputEmail" name="username" class="form-control" placeholder="Email address" required autofocus>
-                                        <label for="inputEmail">Email address</label>
-                                    </div>
-
-                                    <div class="form-label-group">
-                                        <input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required>
-                                        <label for="inputPassword">Password</label>
-                                    </div>
-
-                                    <div class="custom-control custom-checkbox mb-3">
-                                        <input type="checkbox" class="custom-control-input" id="customCheck1">
-                                        <label class="custom-control-label" for="customCheck1">Remember password</label>
-                                    </div>
-                                    <button class="btn btn-lg btn-primary btn-block text-uppercase" name="login" type="submit">Sign
-                                        in</button>
-                                    <hr class="my-4">
-                                </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <video width="1500" height="900" controls>
+      <source src="octank-edu-lecture.mp4" type="video/mp4">
+    Your browser does not support the video tag.
+    </video>
 </body>
+
+
 
 </html>
